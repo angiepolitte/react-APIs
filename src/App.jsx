1a219d0ef28dbe2ac8195b941cb3f286689c1713
Angie <angiepolitte@gmail.com>
2025-05-27 08:10:43 -0500

    axios post request

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,30 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [data, setData] = useState();
 
-  useEffect(() => {
-    setLoading(true);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPost = {
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    };
+
     axios
-      .all([
-        axios.get("https://jsonplaceholder.typicode.com/posts"),
-        axios.get("https://jsonplaceholder.typicode.com/users"),
-      ])
-      .then(
-        axios.spread((posts, users) => {
-          console.log(posts);
-          console.log(users);
-          setData(posts.data);
-          setLoading(false);
-          // throw new Error("Something went wrong!");
-        })
-      )
-      .catch((error) => {
-        console.error("Error fetching data: ", error);
-        setError("Failed to fetch data");
-        setLoading(false);
+      .post("https://jsonplaceholder.typicode.com/posts", newPost)
+      .then((response) => {
+        console.log("New Post Added: ", response.data);
+        setData([response.data]);
       });
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-  if (error) {
-    return <p>{error}</p>;
-  }
-
+  };
   return (
     <>
       <div>
-        <h2>API's</h2>
-        <ul>
-          {data.map((post) => (
-            <li key={post.id}>
-              <p>
-                <strong>{post.title}</strong>
-              </p>
-              <p>{post.body}</p>
-            </li>
-          ))}
-        </ul>
+        <h2>API's in React</h2>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Add Post</button>
+        </form>
       </div>
     </>
   );
@@ -108,4 +84,55 @@ export default App;
 // }
 // if (error) {
 //   return <p>{error}</p>;
+// }
+
+// multiple get request
+
+// useEffect(() => {
+//     setLoading(true);
+//     axios
+//       .all([
+//         axios.get("https://jsonplaceholder.typicode.com/posts"),
+//         axios.get("https://jsonplaceholder.typicode.com/users"),
+//       ])
+//       .then(
+//         axios.spread((posts, users) => {
+//           console.log(posts);
+//           console.log(users);
+//           setData(posts.data);
+//           setLoading(false);
+//           // throw new Error("Something went wrong!");
+//         })
+//       )
+//       .catch((error) => {
+//         console.error("Error fetching data: ", error);
+//         setError("Failed to fetch data");
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   if (loading) {
+//     return <p>Loading...</p>;
+//   }
+//   if (error) {
+//     return <p>{error}</p>;
+//   }
+
+//   return (
+//     <>
+//       <div>
+//         <h2>API's</h2>
+//         <ul>
+//           {data.map((post) => (
+//             <li key={post.id}>
+//               <p>
+//                 <strong>{post.title}</strong>
+//               </p>
+//               <p>{post.body}</p>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </>
+//   );
 // }
